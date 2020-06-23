237e040fe12167a435ecb766ab6a3d514660023d
chethankb007 <67217350+chethankb007@users.noreply.github.com>
2020-06-23 14:02:51 +0530

    Updated Pair Sum in array

--- Problems/OOPS/src/PairSum.java
+++ Problems/OOPS/src/PairSum.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
+/* Given a random integer array A and a number x. Find and print the pair of elements in the array which sum to x.
+Array A can contain duplicate elements.
+While printing a pair, print the smaller element first.
+That is, if a valid pair is (6, 5) print "5 6". There is no constraint that out of 5 pairs which have to be printed in 1st line. You can print pairs in any order, just be careful about the order of elements in a pair.
+
+*/
+
 public class PairSum {
 	
 	public static int count(int[] arr,int elem1,int elem2)
